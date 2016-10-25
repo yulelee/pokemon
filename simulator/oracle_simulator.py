ed644c0b4590817b60b2fffb8e6b8b7564bf04a9
yulelee <yulelee@stanford.edu>
2016-10-25 14:54:56 -0700

    add support for simulating random_agent

--- simulator/oracle_simulator.py
+++ simulator/oracle_simulator.py
@@ -2,18 +2,24 @@ import sys
 sys.path.insert(0, './../agents')
 import oracle_agent
 import random_board
+import random_agent
 
-rb = random_board.random_board(50, verbose = False)
-total_rewards_received = 0
-total_pokemon_num_catched = 0
+# oracle agent and random agent has the same interface, so we can use
+# the same simualtor to simulate them
 
-for _ in range(3000):
-    (rewards, catched_num, _) = rb.move_agent(oracle_agent.oracle_agent.make_decision(rb))
-    total_rewards_received += rewards
-    total_pokemon_num_catched += catched_num
+def simulate(agent):
+    rb = random_board.random_board(50, verbose = False)
+    total_rewards_received = 0
+    total_pokemon_num_catched = 0
 
-print rb.total_spawn_score, total_rewards_received, (total_rewards_received * 1.0 / rb.total_spawn_score)
-print rb.total_spawn_num, total_pokemon_num_catched, (total_pokemon_num_catched * 1.0 / rb.total_spawn_num)
+    for _ in range(3000):
+        (rewards, catched_num, _) = rb.move_agent(agent.make_decision(rb))
+        total_rewards_received += rewards
+        total_pokemon_num_catched += catched_num
 
+    print rb.total_spawn_score, total_rewards_received, (total_rewards_received * 1.0 / rb.total_spawn_score)
+    print rb.total_spawn_num, total_pokemon_num_catched, (total_pokemon_num_catched * 1.0 / rb.total_spawn_num)
 
+simulate(oracle_agent.oracle_agent)
+simulate(random_agent.Random_agent)
 

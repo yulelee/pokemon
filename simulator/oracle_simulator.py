aec04165a9f6484245f298a80153d59f8bc5f3ed
yulelee <yulelee@stanford.edu>
2016-10-25 18:06:52 -0700

    minor changes

--- simulator/oracle_simulator.py
+++ simulator/oracle_simulator.py
@@ -3,12 +3,13 @@ sys.path.insert(0, './../agents')
 import oracle_agent
 import random_board
 import random_agent
+import regional_board
 
 # oracle agent and random agent has the same interface, so we can use
 # the same simualtor to simulate them
 
-def simulate(agent):
-    rb = random_board.random_board(50, verbose = False)
+def simulate(board, agent):
+    rb = board(50, verbose = False)
     total_rewards_received = 0
     total_pokemon_num_catched = 0
 
@@ -20,6 +21,9 @@ def simulate(agent):
     print rb.total_spawn_score, total_rewards_received, (total_rewards_received * 1.0 / rb.total_spawn_score)
     print rb.total_spawn_num, total_pokemon_num_catched, (total_pokemon_num_catched * 1.0 / rb.total_spawn_num)
 
-simulate(oracle_agent.oracle_agent)
-simulate(random_agent.Random_agent)
+simulate(random_board.Random_board, oracle_agent.Oracle_agent)
+simulate(random_board.Random_board, random_agent.Random_agent)
+
+simulate(regional_board.Regional_board, oracle_agent.Oracle_agent)
+simulate(regional_board.Regional_board, random_agent.Random_agent)
 

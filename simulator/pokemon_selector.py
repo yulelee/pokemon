acf1502b6ccfadf95cfcf97d0fe3cb0c524aed4e
yulelee <yulelee@stanford.edu>
2016-11-11 16:11:04 -0800

    decreased the overall spawned scores

--- simulator/pokemon_selector.py
+++ simulator/pokemon_selector.py
@@ -16,20 +16,26 @@ class pokemon_selector():
         # the probability that a neighbor appears along side with the initial spawn
         self.neighbor_rate = neighbor_rate
 
-        # a list of all the pokemon ids, might not be start from 1
-        self.pokemon_id_list = [entry[0] for entry in self.freq_table]
-
-        # a list of all the frequencies, might not be start from pokemon_id 1,
-        # but is consistent with the self.pokemon_id_list
-        self.pokemon_frequency = [entry[3] for entry in self.freq_table]
+        # a dict of pokemon frequencies
+        self.pokemon_frequency = {entry[0]: entry[3] for entry in self.freq_table}
 
         # a random selector, selector from a list of integers, with the coresponding probabilities
         # the pokemon_id stored in those selectors are the real ids, not the list index
-        self.init_selector = stats.rv_discrete(values = (self.pokemon_id_list, self.pokemon_frequency))
+        self.init_selector = stats.rv_discrete(values = (self.pokemon_frequency.keys(), self.pokemon_frequency.values()))
+        
+        self.neighbor_selector = {}
+        for pokemon_id in self.pokemon_frequency.keys():
+            neighbor_prob = {}
+            for neighbor_id in range(1, len(self.pokemon_frequency) + 1):
+                if self.neighbors_stats[pokemon_id - 1][neighbor_id - 1] > 0:
+                    neighbor_prob[neighbor_id] = self.neighbors_stats[pokemon_id - 1][neighbor_id - 1] * self.pokemon_frequency[neighbor_id]
+            
+            sum_p = sum(neighbor_prob.values())
+            for id in neighbor_prob.keys():
+                neighbor_prob[id] /= sum_p
 
-        self.neighbor_selector = [stats.rv_discrete(values = (list(range(1, len(self.pokemon_id_list) + 1)), \
-                [neighbor_count * 1.0 / sum(self.neighbors_stats[index]) for neighbor_count in self.neighbors_stats[index]])) \
-                for index in range(len(self.pokemon_id_list))]
+            self.neighbor_selector[pokemon_id] = stats.rv_discrete(values = (neighbor_prob.keys(), neighbor_prob.values()))
+
 
     # spawn the first pokemon, based on the overall probability of appearance of all the pokemons
     # and then choose to spawn neighbors alongs side the initial pokemon, with the probobilit of
@@ -47,8 +53,10 @@ class pokemon_selector():
         while True:
             result.append([pokemon_id, self.pokedex[pokemon_id - 1]])
             if random.random() > curr_prob: break
-            pokemon_id = self.neighbor_selector[pokemon_id - 1].rvs()
+            pokemon_id = self.neighbor_selector[pokemon_id].rvs()
             curr_prob *= self.neighbor_rate
+
+        #print result
 
         return result
 

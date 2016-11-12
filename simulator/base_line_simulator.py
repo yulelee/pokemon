bc3791ea1e29d396cde14eadafeaeb556ed0bb12
yulelee <yulelee@stanford.edu>
2016-11-11 16:12:45 -0800

    added support to simulate the imporved_agent

--- simulator/base_line_simulator.py
+++ simulator/base_line_simulator.py
@@ -3,23 +3,19 @@ sys.path.insert(0, './../agents')
 import random_board
 import regional_board
 import base_line_agent
-
-
-
-
+import improved_agent
 
 # state for the problem is defined to be tuple (agent_position, radar)
 
-
-def simulate(board):
+def simulate(board, agent):
     bd = board(50, verbose = False)
-    agent = base_line_agent.Base_line_agent(bd.possible_moves)
+    agent.legal_actions = bd.possible_moves
     total_rewards_received = 0
     total_pokemon_num_catched = 0
 
     old_state = (bd.agent_position, [])
 
-    for _ in range(6000):
+    for _ in range(8000):
         action = agent.get_action(old_state)
         (reward, caught_num, radar) = bd.move_agent(action)
         agent.incorperate_feedback(old_state, action, 1, (bd.agent_position, radar))
@@ -32,8 +28,11 @@ def simulate(board):
     print bd.total_spawn_score, total_rewards_received, (total_rewards_received * 1.0 / bd.total_spawn_score)
     print bd.total_spawn_num, total_pokemon_num_catched, (total_pokemon_num_catched * 1.0 / bd.total_spawn_num)
 
-simulate(random_board.Random_board)
-simulate(regional_board.Regional_board)
+simulate(random_board.Random_board, base_line_agent.Base_line_agent())
+simulate(regional_board.Regional_board, base_line_agent.Base_line_agent())
+
+simulate(random_board.Random_board, improved_agent.Improved_agent())
+simulate(regional_board.Regional_board, improved_agent.Improved_agent())
 
 #import cProfile
 #import re
